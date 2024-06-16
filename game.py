bd5774bf2f77ad9f7661e688193b56b65e3944d2
Krystian Sumara <potatocatkris@gmail.com>
2024-06-15 21:43:50 -0500

    Add files via upload

--- game.py
+++ game.py
@@ -60,20 +60,18 @@ def help_menu():
     print('- good luck, have fun!              ')
     title_screen()
 
-    # Game Functionality #
+# Game Functionality #
 def start_game():
 
 # Map #
-"""
+#
 # player starts at b3
-----------------
-|a1|a2|a3|a4|a5|
-----------------
-|b1|b2|b3|b4|b5|
-----------------
-|c1|c2|c3|c4|c6|
-----------------
-|d1|d2|d3|d4|d5|
-----------------
-
-"""
+# ----------------
+# |a1|a2|a3|a4|a5|
+# ----------------
+# |b1|b2|b3|b4|b5|
+# ----------------
+# |c1|c2|c3|c4|c5|
+# ----------------
+# |d1|d2|d3|d4|d5|
+# ----------------
